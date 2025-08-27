9943ed78d41c354b35340cca35ee495f9d8bcc5e
Ragul116 <ragulranjith1106@gmail.com>
2025-08-27 17:01:59 +0530

    add allowed hosts

--- profile_uploader/settings.py
+++ profile_uploader/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-plp^k#^p2jh6^6hwapz6-0(zziqojm#9mlx15cf(g$uxq_$m=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["profile-picture-uploader.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
